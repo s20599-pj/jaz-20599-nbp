529e1e55d931b3e957c8bc6ad32550abc7f2828c
Rominski, Kamil <s20599@pjwstk.edu.pl>
2021-07-03 17:16:55 +0200

    Controller documentation

--- src/main/java/pjatk/jaz20599nbp/Controller/NbpController.java
+++ src/main/java/pjatk/jaz20599nbp/Controller/NbpController.java
@@ -1,5 +1,9 @@
 package pjatk.jaz20599nbp.Controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +22,19 @@ public class NbpController
         this.nbpService = nbpService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Nie znaleziono waluty"),
+            @ApiResponse(code = 400, message = "Błąd zapytania"),
+            @ApiResponse(code = 200, message = "OK")
+    })
+    @ApiOperation(value = "Pobierz sredni kurs waluty z podanego zakresu daty", notes = "Podaj walutę, datę początkową oraz końcową zakresu")
     @GetMapping("/{currency}/{start_date}/{end_date}")
     public ResponseEntity<Double> getAverageFromSelectedDays(
+            @ApiParam(value = "Waluta, o którą chcesz zapytać", example = "NOK")
             @PathVariable String currency,
+            @ApiParam(value = "Data początkowa", example = "2014-01-31")
             @PathVariable String start_date,
+            @ApiParam(value = "Data końcowa", example = "2014-02-15")
             @PathVariable String end_date)
     {
         return ResponseEntity.ok(nbpService.getAvgRate(currency, start_date, end_date));
---
+++ src/main/java/pjatk/jaz20599nbp/Repository/NbpRepository.java
@@ -0,0 +1,8 @@
+package pjatk.jaz20599nbp.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pjatk.jaz20599nbp.Class.Root;
+
+public interface NbpRepository extends JpaRepository<Root, Long>
+{
+}

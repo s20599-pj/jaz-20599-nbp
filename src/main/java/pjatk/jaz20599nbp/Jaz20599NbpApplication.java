6fba94fc03839bbfc425347b80650c5605cc59c0
Rominski, Kamil <s20599@pjwstk.edu.pl>
2021-07-03 16:10:36 +0200

    Init commit

---
+++ src/main/java/pjatk/jaz20599nbp/Jaz20599NbpApplication.java
@@ -0,0 +1,15 @@
+package pjatk.jaz20599nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz20599NbpApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Jaz20599NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjatk/jaz20599nbp/Jaz20599NbpApplicationTests.java
@@ -0,0 +1,15 @@
+package pjatk.jaz20599nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz20599NbpApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
